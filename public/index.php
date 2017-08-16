9f09ad069cc1a3d6ad62fdb40c8cf5dccab5118e
Krzysztof Sawarzynski <krzysztof.sawarzynski@protonmail.com>
2017-08-16 14:54:41 +0200

    reorganize project structure

--- public/index.php
+++ public/index.php
@@ -1,28 +1,37 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints as Assert;
 
+
+/** @var constant project root */
+define("ROOT", __DIR__ . '/..');
+
+/** @var constant public folder */
+define("PUBLIC_PATH", ROOT . '/public');
+
+/** @var constant path to settings.yml */
+define("SETTINGS_PATH", ROOT . '/settings.yml');
+
+
 $app = new Silex\Application();
 
+// TODO: turn off in producton!
 $app['debug'] = true;
 
-$app['config'] = Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . '/settings.yml'));
+$app['config'] = Symfony\Component\Yaml\Yaml::parse(file_get_contents(SETTINGS_PATH));
 
+// SERVICE PROVIDERS
 $app->register(new Silex\Provider\ValidatorServiceProvider());
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), [
     'db.options' => $app['config']['database'],
 ]);
 
+
 $app->post('/item', function (Request $request) use ($app) {
-    $data = [
-        'src' => $request->get('src'),
-        'rating' => $request->get('rating'),
-        'date' => $request->get('date'),
-    ];
 
     $constraint = new Assert\Collection([
         'src' => [new Assert\NotBlank(), new Assert\Length(['max'=>200]), new Assert\Url()],
@@ -30,15 +39,10 @@ $app->post('/item', function (Request $request) use ($app) {
         'date' => [new Assert\NotBlank, new Assert\DateTime()],
     ]);
 
-    $errors = $app['validator']->validate($data, $constraint);
+    $errors = $app['validator']->validate($request->request->getIterator(), $constraint);
 
-    if (count($errors) > 0) {
-        $errorsMsg = '';
-        foreach ($errors as $error) {
-            $errorsMsg .= $error->getPropertyPath().' '.$error->getMessage()."\n";
-        }
-        return new Response($errorsMsg, 200);
-    }
+    if (count($errors) > 0)
+        return new Response($errors, 200);
 
     $stmt = $app['db']->prepare("INSERT INTO item VALUES (NULL, :src, :rating, :date);");
     $stmt->bindValue("src", $request->get('src'));
@@ -50,10 +54,6 @@ $app->post('/item', function (Request $request) use ($app) {
         return new Response("Poszło", 200);
     else
         return new Response("Nie poszło", 200);
-});
-
-$app->get('/', function () use ($app) {
-    return '<!DOCTYPE html><html><head></head><body>Hello world</body></html>';
 });
 
 
